75a1bd3690b488e2ff62bea5d337ede38ee29c91
Asia <asyayakimova@yahoo.com>
2025-02-21 13:19:28 +0100

    Login in react

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { LoginView } from './LoginView';
+import { useEffect, useState } from "react";
+
 
 function App() {
+ const [user, setUser] = useState();
+  useEffect(() => {
+    setUser(JSON.parse(window.localStorage.getItem("user")));
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          // className="App-link"
+          // href="https://reactjs.org"
+          // target="_blank"
+          // rel="noopener noreferrer"
         >
-          Learn React
+          <LoginView />
+   
         </a>
       </header>
     </div>
---
+++ src/LoginView.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { useEffect, useRef, useState } from "react";
+import "./styleLogin.css";
+
+export function LoginView() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+   const usernameRef = useRef();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("handle submit");
+  };
+  useEffect(() => {
+    usernameRef.current.focus();
+  }, []);
+
+  return (
+    <div className="login_container">
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit} className="form_container">
+        <div className="form_controls">
+          <label htmlFor="username">Username</label>
+          <input
+            ref={usernameRef}
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="form_controls">
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="form_controls">
+          <button className="button">Login</button>
+        </div>
+      </form>
+    </div>
+  );
+}
